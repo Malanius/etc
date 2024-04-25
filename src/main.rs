1be6413ccc1a13644c0f8fdb172fb66dd933c9dc
Malanius Privierre <malaniusprivierre@gmail.com>
2024-04-25 17:44:32 +0200

    chore: pack tailwind.css to assets

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,10 @@
 use dioxus::prelude::*;
 use log::LevelFilter;
 
+// Urls are relative to your Cargo.toml file
+const _TAILWIND_URL: &str = manganis::mg!(file("public/tailwind.css"));
+
+
 fn main() {
     // Init debug
     dioxus_logger::init(LevelFilter::Info).expect("failed to init logger");
@@ -21,9 +25,21 @@ fn App() -> Element {
         div { id: "links",
             a { target: "_blank", href: "https://dioxuslabs.com/learn/0.5/", "📚 Learn Dioxus" }
             a { target: "_blank", href: "https://dioxuslabs.com/awesome", "🚀 Awesome Dioxus" }
-            a { target: "_blank", href: "https://github.com/dioxus-community/", "📡 Community Libraries" }
-            a { target: "_blank", href: "https://github.com/DioxusLabs/dioxus-std", "⚙️ Dioxus Standard Library" }
-            a { target: "_blank", href: "https://marketplace.visualstudio.com/items?itemName=DioxusLabs.dioxus", "💫 VSCode Extension" }
+            a {
+                target: "_blank",
+                href: "https://github.com/dioxus-community/",
+                "📡 Community Libraries"
+            }
+            a {
+                target: "_blank",
+                href: "https://github.com/DioxusLabs/dioxus-std",
+                "⚙️ Dioxus Standard Library"
+            }
+            a {
+                target: "_blank",
+                href: "https://marketplace.visualstudio.com/items?itemName=DioxusLabs.dioxus",
+                "💫 VSCode Extension"
+            }
             a { target: "_blank", href: "https://discord.gg/XgGxMSkvUM", "👋 Community Discord" }
         }
     }
